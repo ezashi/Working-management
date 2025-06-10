d89bef3605440366714fe35a9f9048507e7ed873
ezashi <soma3070@icloud.com>
2025-06-10 16:01:28 +0900

    feat: AdminSeeder.phpでUserモデルを使用するように修正
    AdminSeeder.phpにおいて、Adminモデルの代わりにUserモデルを使用して管理者ユーザーを作成するように変更しました。

--- src/database/seeders/AdminSeeder.php
+++ src/database/seeders/AdminSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,10 +15,11 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::create([
+        User::create([
             'name' => 'admin_user',
             'email' => 'admin@example.com',
             'password' => Hash::make('12345678'),
+            'role' => 'admin',
         ]);
     }
 }
